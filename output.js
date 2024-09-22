8c71678e224eeec08ebb17b5468447d05f35b8b2
action <action@github.com>
2024-09-22 09:11:16 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Thu Sep 19 2024 11:23:08 GMT+0000 (Coordinated Universal Time)
+//Sun Sep 22 2024 09:11:16 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("大潮");
