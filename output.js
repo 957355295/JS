c1cf22632bb172a77fa6437c329bb78e49ec7609
action <action@github.com>
2025-02-11 10:45:14 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Fri Feb 07 2025 06:31:34 GMT+0000 (Coordinated Universal Time)
+//Tue Feb 11 2025 10:45:13 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("发送验证码");
